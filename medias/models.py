3e225facdd3c2acac809da225a2571954a8a6f47
Sangha Lee <sanghalee629@gmail.com>
2024-04-19 22:03:53 +0900

    add medias app

---
+++ medias/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Photo(CommonModel):
+    """Photo model definition"""
+
+    file = models.ImageField()
+    description = models.CharField(
+        max_length=140,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Photo File"
+
+
+class Video(CommonModel):
+    """Video model definition"""
+
+    file = models.FileField()
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Video File"
