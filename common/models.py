01527cd306f14100ff00e44d27edb6f35fb748f5
Sangha Lee <sanghalee629@gmail.com>
2024-04-19 13:47:31 +0900

    add common app

---
+++ common/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class CommonModel(models.Model):
+    """Common model definition"""
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateField(
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
