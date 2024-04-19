72e871ef2e1dc9a15c9db9e3e504a9872fce71db
Sangha Lee <sanghalee629@gmail.com>
2024-04-19 14:26:53 +0900

    add experiences app

---
+++ experiences/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from common.models import CommonModel
+
+
+# Create your models here.
+class Experience(CommonModel):
+    """Experience model definition"""
+
+    country = models.CharField(
+        max_length=50,
+        default="대한민국",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    name = models.CharField(
+        max_length=250,
+    )
+    host = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    price = models.PositiveIntegerField()
+    address = models.CharField(
+        max_length=250,
+    )
+    start = models.TimeField()
+    end = models.TimeField()
+    description = models.TextField()
+    perks = models.ManyToManyField("experiences.Perk")
+
+    def __str__(self):
+        return self.name
+
+
+class Perk(CommonModel):
+    """What is included in an Experience"""
+
+    name = models.CharField(
+        max_length=100,
+    )
+    detail = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+    explanation = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
