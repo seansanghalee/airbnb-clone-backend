dd6cf777aa158f91d76184ada0ad7421dd8e5206
Sangha Lee <sanghalee629@gmail.com>
2024-04-24 09:47:59 +0900

    add related_name and custom filters

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,10 +2,36 @@ from django.contrib import admin
 from reviews.models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"  # in the url
+
+    def lookups(self, request, modeladmin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+            ("nice", "Nice"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(modeladmin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
     list_display = (
         "name",
         "price",
         "kind",
         "total_amenities",
+        "rating",
         "owner",
     )
     list_filter = (
@@ -17,6 +26,10 @@ class RoomAdmin(admin.ModelAdmin):
         "pet_friendly",
         "kind",
         "amenities",
+    )
+    search_fields = (
+        "name",
+        "price",
     )
 
     def total_amenities(self, room):
