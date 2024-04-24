c19b8826082f20be310a9d3c5a16378e4cd0e4cd
Sangha Lee <sanghalee629@gmail.com>
2024-04-24 12:31:23 +0900

    practice django templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Room
+
 
 # Create your views here.
+def see_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Rooms",
+        },
+    )
+
+
+def see_room(request, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        return render(
+            request,
+            "room.html",
+            {
+                "room": room,
+            },
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room.html",
+            {
+                "not_found": True,
+            },
+        )
